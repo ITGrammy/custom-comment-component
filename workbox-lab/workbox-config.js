5d240afb303449bc61423df5e13d98d5fb730c76
Monique <yofinancialfreedommatters@gmail.com>
2022-08-27 17:20:17 -0400

    Workbox for installing and offline workn it works!

---
+++ workbox-lab/workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "project/",
+  "globPatterns": [
+    "**/*.{jpg,svg,html,js,css,json}"
+  ],
+  "swDest": "project\\sw.js",
+  "swSrc": "project/src/sw.js"
+};
