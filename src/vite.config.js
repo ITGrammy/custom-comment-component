df716e742423914706cd8a302f8479ee6c91e224
Monique <yofinancialfreedommatters@gmail.com>
2022-09-05 12:02:04 -0400

    Changes deleted db test from html and commented  outplugins  in vconfig

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,18 +2,18 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      strategies: "injectManifest",
-      injectManifest: {
-        globPatterns: ["**/*.html"],
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
+  // plugins: [
+  //   VitePWA({
+  //     registerType: "autoUpdate",
+  //     strategies: "injectManifest",
+  //     injectManifest: {
+  //       globPatterns: ["**/*.html"],
+  //     },
+  //     devOptions: {
+  //       enabled: true,
+  //     },
+  //   }),
+  // ],
   build:{
     outDir:"../dist"
   }
